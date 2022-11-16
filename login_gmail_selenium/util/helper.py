90e07c4e6224dbfdb4c9011124a1498914fd37b9
nguyenThanhDg <nguyenthanhdungktm@gmail.com>
2022-11-16 16:19:43 +0700

    remove content and add gitignore

--- login_gmail_selenium/util/helper.py
+++ login_gmail_selenium/util/helper.py
@@ -1,13 +1,10 @@
 import random
-
-import requests
 import math
 
 import login_gmail_selenium.common.constant as Constant
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-import fake_headers.browsers as browsers
 from time import sleep
 from random import randint, uniform
 import selenium.webdriver.support.expected_conditions as EC
@@ -16,17 +13,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def sleep_for(period):
     sleep(randint(period[0], period[1]))
-
-
-def scroll_to_element(driver, element, retry=Constant.RETRY):
-    for i in range(retry):
-        sleep_for(Constant.SHORT_WAIT)
-        driver.execute_script("arguments[0].scrollIntoView();", element)
-        desired_y = (element.size['height'] / 2) + element.location['y']
-        current_y = (driver.execute_script('return window.innerHeight') / 2) + driver.execute_script(
-            'return window.pageYOffset')
-        scroll_y_by = desired_y - current_y
-        driver.execute_script("window.scrollBy(0, arguments[0]);", scroll_y_by)
 
 
 def type_text(driver, text, xpath, custom_enter=None, paste_text=Constant.PASTE_PERCENTAGE, loading=False):
@@ -56,15 +42,6 @@ def ensure_click(driver, xpath, retry=Constant.RETRY, refresh=False):
         driver.find_element(By.XPATH, xpath).click()
     execute_with_retry(driver, click_search, retry=retry, refresh=refresh)
     sleep_for(Constant.SHORT_WAIT)
-
-
-def update_chrome_version():
-    link = 'https://gist.githubusercontent.com/MShawon/29e185038f22e6ac5eac822a1e422e9d/raw/versions.txt'
-
-    output = requests.get(link, timeout=60).text
-    chrome_versions = output.split('\n')
-
-    browsers.chrome_ver = chrome_versions
 
 
 def execute_with_retry(driver, callback, error=Exception, retry=Constant.RETRY, with_result=False, refresh=False):
@@ -103,12 +80,4 @@ def ensure_find_element(driver, xpath):
 
     return execute_with_retry(driver, get_element, with_result=True)
 
-
-def random_scroll(driver):
-    sleep_for(Constant.SHORT_WAIT)
-    for i in range(7):
-        sleep_for(Constant.WIDE_WAIT)
-        action = random.choice([Keys.DOWN, Keys.END, Keys.UP, Keys.HOME, Keys.SPACE,
-                                Keys.F11, Keys.ARROW_DOWN, Keys.ARROW_UP, Keys.F5])
-        driver.find_element(By.TAG_NAME, 'body').send_keys(action)
 
