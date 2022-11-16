fbc41d68757ca922ba4a090f6ea1639eb5722fe8
nguyenThanhDg <nguyenthanhdungktm@gmail.com>
2022-11-16 14:21:58 +0700

    delete redundant file

--- build/lib/login_gmail_selenium/login.py
+++
@@ -1,6 +0,0 @@
-from login_gmail_selenium.util.profile import ChromeProfile
-
-
-def login(email, password, backup_email):
-    profile = ChromeProfile(email, password, backup_email)
-    profile.start()
