c6ec8d5fcf66bdee74f886fb071ebe05c8754cb7
nguyenThanhDg <nguyenthanhdungktm@gmail.com>
2022-12-02 15:27:28 +0700

    update with multi version delete login.py

--- login_gmail_selenium/login.py
+++
@@ -1,9 +0,0 @@
-from util.profile import ChromeProfile
-
-def login(email, password, backup_email):
-    profile = ChromeProfile(email, password, backup_email)
-    profile.start()
-    profile.get_version_browser()
-
-if __name__ == '__main__':
-    login('terabox269@gmail.com', 'dung@123', 'ngudfwsfd@gmail.com')
