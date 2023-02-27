4ae6171a7f9b0d1da9e90917c6122c39dfb8e18d
ngThanhDg <nguyenthanhdungktm@gmail.com>
2023-02-27 15:33:08 +0700

    update change password 2

--- test/test.py
+++ test/test.py
@@ -1,9 +1,9 @@
 from login_gmail_selenium.util.profile import ChromeProfile
 import time
-import os
+
 
 if __name__ == '__main__':
-    profile = ChromeProfile('rungcaengwrwuthi15@gmail.com', 'p8S9Qec1A4', 'thaddeus050756@hotmail.com')
+    profile = ChromeProfile('lrod44536@gmail.com', 'Pt6o4FWy8k', 'karasalab906008@hotmail.com')
     driver = profile.retrieve_driver()
     profile.start()
     driver.get('https://www.google.com/')
